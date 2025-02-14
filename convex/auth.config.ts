3e039375d063bc585c8d9a9b550912d7ebf04d6d
Shantanu-Tiwari <shantanutiwari240704@gmail.com>
2025-02-15 00:36:04 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+        providers: [
+            {
+                domain: "https://eager-racer-7.clerk.accounts.dev/",
+                applicationID: "convex",
+            },
+        ],
+    };
